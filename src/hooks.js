3cb3c425e83621cea573b4eed84cf5cf7af15e6f
Saif <saifisami2005@gmail.com>
2024-12-12 02:27:34 -0500

    Minor fix.

--- src/hooks.js
+++ src/hooks.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const useFlip = (originalState = true) => {
-    const [isFlipped, setFlipped] = useState(initialFlipState), flip = () => setFlipped(up => !up);
+    const [isFlipped, setFlipped] = useState(originalState), flip = () => setFlipped(up => !up);
     return [isFlipped, flip];
 }
 

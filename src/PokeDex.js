b2f61b457e64ed7de6f96b1ecea28fec4e1d3ee4
Saif <saifisami2005@gmail.com>
2024-12-12 02:37:25 -0500

    More refactorization.

--- src/PokeDex.js
+++ src/PokeDex.js
@@ -20,16 +20,7 @@ function PokeDex() {
       </div>
       <div className="PokeDex-card-area">
         {pokemon.map(cardData => (
-          <PokemonCard
-            key={cardData.id}
-            front={cardData.sprites.front_default}
-            back={cardData.sprites.back_default}
-            name={cardData.name}
-            stats={cardData.stats.map(stat => ({
-              value: stat.base_stat,
-              name: stat.stat.name
-            }))}
-          />
+          <PokemonCard key={cardData.id} {...cardData}/>
         ))}
       </div>
     </div>

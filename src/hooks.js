22802d5789b5695781ba05feed705790465cef5d
Saif <saifisami2005@gmail.com>
2024-12-12 02:14:51 -0500

    Created useAxios and useLocalStorage.

--- src/hooks.js
+++ src/hooks.js
@@ -6,4 +6,29 @@ const useFlip = (originalState = true) => {
     return [isFlipped, flip];
 }
 
-export { useFlip };
+const useAxios = (lsKey, baseUrl) => {
+    const [responses, setResponses] = useLocalStorage(lsKey);
+
+    const addResponseData = async (formatter = data => data, restOfUrl = "") => {
+        const response = await axios.get(`${baseUrl}${restOfUrl}`);
+        setResponses(data => [...data, formatter(response.data)]);
+      };
+    
+      const clearResponses = () => setResponses([]);
+      return [responses, addResponseData, clearResponses];
+}
+
+const useLocalStorage = (key, initialValue = []) => {
+    if (localStorage.getItem(key)) {
+      initialValue = JSON.parse(localStorage.getItem(key));
+    }
+    const [value, setValue] = useState(initialValue);
+  
+    useEffect(() => {
+      localStorage.setItem(key, JSON.stringify(value));
+    }, [value, key]);
+  
+    return [value, setValue];
+  }
+
+export { useFlip , useAxios, useLocalStorage};

2ca1eaa681ebde3e1c79ff959b9b2aeaeae8e9ca
Saif <saifisami2005@gmail.com>
2024-12-12 02:15:26 -0500

    Slight reformatting.

--- src/hooks.js
+++ src/hooks.js
@@ -12,23 +12,23 @@ const useAxios = (lsKey, baseUrl) => {
     const addResponseData = async (formatter = data => data, restOfUrl = "") => {
         const response = await axios.get(`${baseUrl}${restOfUrl}`);
         setResponses(data => [...data, formatter(response.data)]);
-      };
-    
-      const clearResponses = () => setResponses([]);
-      return [responses, addResponseData, clearResponses];
+    };
+
+    const clearResponses = () => setResponses([]);
+    return [responses, addResponseData, clearResponses];
 }
 
 const useLocalStorage = (key, initialValue = []) => {
     if (localStorage.getItem(key)) {
-      initialValue = JSON.parse(localStorage.getItem(key));
+        initialValue = JSON.parse(localStorage.getItem(key));
     }
     const [value, setValue] = useState(initialValue);
-  
-    useEffect(() => {
-      localStorage.setItem(key, JSON.stringify(value));
-    }, [value, key]);
-  
-    return [value, setValue];
-  }
 
-export { useFlip , useAxios, useLocalStorage};
+    useEffect(() => {
+        localStorage.setItem(key, JSON.stringify(value));
+    }, [value, key]);
+
+    return [value, setValue];
+}
+
+export { useFlip, useAxios, useLocalStorage };

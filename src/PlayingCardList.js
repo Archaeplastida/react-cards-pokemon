3ec319cf4a365d7fc05df16d31c338688baa6519
Saif <saifisami2005@gmail.com>
2024-12-12 02:25:35 -0500

    Minor fix.

--- src/PlayingCardList.js
+++ src/PlayingCardList.js
@@ -14,7 +14,7 @@ function CardTable() {
       <h3>Pick a card, any card!</h3>
       <div>
         <button onClick={addCard}>Add a playing card!</button>
-        <button onClick={clearCards}>Clear the table</button>
+        <button onClick={clearCards}>Clear table</button>
       </div>
       <div className="PlayingCardList-card-area">
         {cards.map(cardData => (
